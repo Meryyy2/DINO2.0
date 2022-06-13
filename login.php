10706d40028bd11f03f04f3666f1c67fac2b6bda
JMugetsu <76715127+JMugetsu@users.noreply.github.com>
2022-06-13 14:24:16 -0600

    dsadsad

--- login.php
+++ login.php
@@ -301,15 +301,17 @@
     <script src="././js/plugins.js"></script>
     <script src="././js/main.js"></script>
     
-    <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
-    async defer>
-</script>
-    
     <script type="text/javascript">
   var onloadCallback = function() {
     alert("grecaptcha is ready!");
   };
 </script>
+    
+    <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+    async defer>
+</script>
+    
+    
 
 </body>
     
