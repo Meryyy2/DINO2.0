7c072d1a2ff9e6dacea8a1d048028cdf6ab83314
Maria Valverde <meryvaverde2@gmail.com>
2022-06-13 04:24:25 -0600

    estoy harta

--- index.php
+++ index.php
@@ -1240,7 +1240,7 @@
                                 <img src="img/dino/yo.jpeg" alt="#">
                             </div>
                             <h3>"Gerente General"</h3>
-                            <h5>- Maria Valverde</h5>
+                            <h5>-Maria Valverde</h5>
                         </div>
                         <div class="single_client_review">
                             <div class="client_img">
@@ -1355,38 +1355,38 @@
 	<!-- JS here -->
 	
 		<!-- All JS Custom Plugins Link Here here -->
-        <script src="./js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
 		<!-- Jquery, Popper, Bootstrap -->
-		<script src="./js/vendor/jquery-1.12.4.min.js"></script>
-        <script src="./js/popper.min.js"></script>
-        <script src="./js/bootstrap.min.js"></script>
+		<script src="js/vendor/jquery-1.12.4.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
 	    <!-- Jquery Mobile Menu -->
-        <script src="./js/jquery.slicknav.min.js"></script>
+        <script src="js/jquery.slicknav.min.js"></script>
 
 		<!-- Jquery Slick , Owl-Carousel Plugins -->
-        <script src="./js/owl.carousel.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
         <script src="./js/slick.min.js"></script>
 
 		<!-- One Page, Animated-HeadLin -->
-        <script src="./js/wow.min.js"></script>
-		<script src="./js/animated.headline.js"></script>
-        <script src="./js/jquery.magnific-popup.js"></script>
+        <script src="js/wow.min.js"></script>
+		<script src="js/animated.headline.js"></script>
+        <script src="js/jquery.magnific-popup.js"></script>
 
 		<!-- Scrollup, nice-select, sticky -->
-        <script src="./js/jquery.scrollUp.min.js"></script>
-        <script src="./js/jquery.nice-select.min.js"></script>
-		<script src="./js/jquery.sticky.js"></script>
+        <script src="js/jquery.scrollUp.min.js"></script>
+        <script src="js/jquery.nice-select.min.js"></script>
+		<script src="js/jquery.sticky.js"></script>
         
         <!-- contact js -->
-        <script src="./js/contact.js"></script>
-        <script src="./js/jquery.form.js"></script>
-        <script src="./js/jquery.validate.min.js"></script>
-        <script src="./js/mail-script.js"></script>
-        <script src="./js/jquery.ajaxchimp.min.js"></script>
+        <script src="js/contact.js"></script>
+        <script src="js/jquery.form.js"></script>
+        <script src="js/jquery.validate.min.js"></script>
+        <script src="js/mail-script.js"></script>
+        <script src="js/jquery.ajaxchimp.min.js"></script>
         
 		<!-- Jquery Plugins, main Jquery -->	
-        <script src="./js/plugins.js"></script>
-        <script src="./js/main.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
         
     </body>
 </html>
